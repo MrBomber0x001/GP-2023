2afd28a210e545e3aedb3dea9398ca923cf01373
Yousef Meska <mrbomberboy123@gmail.com>
2023-02-26 19:58:41 +0200

    added signin

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,13 @@
 export const signup = (req, res) => {
     res.send("welcome");
 }
+
+/**
+ * 
+ * @author Yousef Meska
+ * @description SignIn route
+ * @param {req, res}
+ */
+export const signIn = (req, res) => {
+    // bla bla
+}
