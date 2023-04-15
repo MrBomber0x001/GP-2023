bf97cc6ae600756c590aa2b3d2dcf1b192331c05
MohamedSherif <mohamedsh2782@gmail.com>
2023-04-15 14:51:35 +0200

    added migration file

---
+++ prisma/migrations/20230415124723_added_created_at_updated_at_to_category_sub_category_service/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Service` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Sub_Category` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Sub_Category" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -79,3 +79,4 @@ export const isAdmin = async (req, res, next) => {
         next(error);
     }
 };
+
