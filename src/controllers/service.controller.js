6bb4bbe4ae6e9c4f945c13500c64d69eff1b68b3
Eslam Alameldin <eashraf36@gmail.com>
2023-07-11 19:10:12 +0300

    adding get services by subCategory id route

---
+++ src/controllers/service.controller.js
@@ -0,0 +1,24 @@
+import prisma from "../config/prisma.js";
+import { BadRequestError, httpStatusCodes } from "../error/index.js";
+
+export const getServicesBySubCatId = async (req, res, next) => {
+    try {
+        const { subCatId } = req.params;
+
+        if (!subCatId) {
+            throw new BadRequestError("Sub Category Id is required");
+        }
+
+        // get all services by sub category id
+
+        const services = await prisma.service.findMany({
+            where: {
+                subCatId: subCatId,
+            },
+        });
+
+        res.status(httpStatusCodes.OK).json(services);
+    } catch (error) {
+        next(error);
+    }
+};
---
+++ src/routes/v1/service.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import {
+    getServicesBySubCatId,
+} from "../../controllers/service.controller.js";
+
+const router = express.Router();
+
+router.route("/:subCatId").get(getServicesBySubCatId);
+
+export default router;
